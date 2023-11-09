0eca03d6d851b871362264d0d9cd10a6db68a561
Ben Mubanga Ngosa <85124955+MubangaNgosa@users.noreply.github.com>
2023-11-09 15:50:54 -0800

    Update jobPool.cc

--- core/jobPool.cc
+++ core/jobPool.cc
@@ -1,39 +1,33 @@
 #include <zmq.hpp>
-#include <string>
 #include <iostream>
-#include <thread>
-#include <chrono>
+#include <vector>
+#include <string>
 
 int main() {
     zmq::context_t context(1);
-    zmq::socket_t socket(context, ZMQ_ROUTER); // Router socket to communicate with multiple clients
+    zmq::socket_t socket(context, ZMQ_ROUTER);
     socket.bind("tcp://*:5555");
 
-    int jobNumber = 1;
+    uint64_t num_tasks = 100; // For example, total number of tasks to distribute
+    int num_worker_nodes = 5; // For example, number of worker nodes
+    std::vector<uint64_t> worker_task_index(num_worker_nodes, 0);
 
     while (true) {
-        zmq::message_t request;
-
-        // Wait for next request from client
         zmq::message_t identity;
-        socket.recv(identity);
-        socket.recv(request);
+        zmq::message_t request;
+        socket.recv(&identity);
+        socket.recv(&request);
 
-        std::string request_str(static_cast<char*>(request.data()), request.size());
+        std::string worker_id(static_cast<char*>(request.data()), request.size());
 
-        if (request_str == "request job") {
-            std::cout << "Received job request" << std::endl;
+        // ... rest of your code ...
 
-            // Send job number as string
-            std::string jobString = std::to_string(jobNumber++);
-            zmq::message_t reply(jobString.size());
-            memcpy(reply.data(), jobString.c_str(), jobString.size());
+        // Use "%lu" or the correct format specifier for your variable type
+        snprintf((char *)task.data(), 20, "%lu", worker_task_index[worker_index]++);
 
-            socket.send(identity, zmq::send_flags::sndmore);
-            socket.send(reply);
-
-            std::cout << "Sent job " << jobString << std::endl;
-        }
+        // Send the task message
+        socket.send(identity, ZMQ_SNDMORE);
+        socket.send(task, 0);
     }
     return 0;
 }
